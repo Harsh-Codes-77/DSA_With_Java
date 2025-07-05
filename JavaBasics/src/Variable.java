deb9da3c12cf30fe0ab0e0593a459d846e61d040
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-05 15:51:58 +0530

    Variables in Java

---
+++ JavaBasics/src/Variable.java
@@ -0,0 +1,15 @@
+public class Variable{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        String name = "Harsh Pathak";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+
+//      If you want to change the value of a then =>
+
+        a = 50;
+        System.out.println(a);
+    };
+};
