1e9865bb1197e198f6af0ecb11379eeb8b1aa8b5
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-06 17:16:45 +0530

    Arithmetic Operator

---
+++ Operators in JAVA/src/ArithmeticOperator.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ArithmeticOperator{
+    public static void main(String args[]){
+        int A = 40;
+        int B = 20;
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println((A/B));
+        System.out.println(A%B);
+    }
+}
