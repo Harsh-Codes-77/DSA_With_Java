2707d1db398e3736fe94485a10d7dd1921bad792
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-06 16:54:13 +0530

    Type Promotion

---
+++ JavaBasics/src/TypePromotion.java
@@ -0,0 +1,15 @@
+public class TypePromotion{
+    public static void main(String args[]){
+        char a = 'a';
+        char b = 'b';
+        System.out.println((int)(b));
+        System.out.println((int)(a));
+        System.out.println(b-a);
+    }
+}
+//
+//     output:
+//
+//        98
+//        97
+//        1
