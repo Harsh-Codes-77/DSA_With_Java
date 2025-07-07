badeddb4732f3bb6b407f5f1e46ae687daa08f87
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-07 17:15:25 +0530

    Conditional Statement

---
+++ Conditional Statements/src/largestOfTwo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class largestOfTwo{
+    public static void main(String args[]){
+        int a = 20;
+        int b = 50;
+        if(a >= b){
+            System.out.println("a is largest of 2");
+        } else{
+            System.out.println("B is largest of 2");
+        }
+    }
+};
