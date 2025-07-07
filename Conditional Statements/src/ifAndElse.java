46b59d9983271ca33e487a221067fcc9da48f2ab
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-07 17:17:28 +0530

    If and Else Statement

---
+++ Conditional Statements/src/ifAndElse.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ifAndElse{
+    public static void main(String args[]){
+        int age = 20;
+        if(age >= 18){
+            System.out.println("Adult: drive, vote");
+        } else{
+            System.out.println("not adult");
+        }
+    }
+};
