90c32de5811fbdde15048faf42b8630c3529d081
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-07 17:58:58 +0530

    Ternary operator

---
+++ Conditional Statements/src/ternary.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ternary{
+    public static void main(String args[]){
+        int marks = 67;
+
+        String reportCard = marks >= 33 ? "Pass" : "Fail";
+        System.out.println(reportCard);
+    }
+}
