96df46560411cddf9f354fd95eabdc2d320fbc5e
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-11 16:28:27 +0530

    Using Functions

---
+++ Functions/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void printHelloWorld() {
+        System.out.print("Hii, ");
+        System.out.println("my name is Harsh");
+    }
+    public static void main(String args[]){
+        printHelloWorld();
+    }
+}
