1a5b750c972e5808f07e68197a00d31d83a52db8
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-11 17:44:34 +0530

    Swapping of two numbers using call by value method

---
+++ Functions/src/swapping.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class swapping{
+    public static void swapNo(int a, int b){
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("Before swap " + "a = : " + a + " b : " + b);
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        swapNo(a, b);
+
+        System.out.println("After swap " + "a : " + a + " b : " + b);
+    }
+}
