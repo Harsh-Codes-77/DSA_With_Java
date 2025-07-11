1e8f42793d36f25a0be7d7e2e658fd66678f9988
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-11 16:32:29 +0530

    Using Functions

--- Functions/src/Main.java
+++ Functions/src/Main.java
@@ -4,8 +4,16 @@ public class Main{
     public static void printHelloWorld() {
         System.out.print("Hii, ");
         System.out.println("my name is Harsh");
-    }
+    };
+
+//    Must include return if you cannot use void otherwise it gives an error.
+
+    public static int printHello(){
+        System.out.print("Return");
+        return 1;
+    };
     public static void main(String args[]){
         printHelloWorld();
+        printHello();
     }
 }
