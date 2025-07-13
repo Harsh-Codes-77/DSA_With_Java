70c45983f1acae53c1a61e28234aa334889da082
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-13 16:30:47 +0530

    Function Overloading

--- Functions/src/functionOverloading.java
+++ Functions/src/functionOverloading.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
 public class functionOverloading{
+
+    //Function overloading using Parameters
     public static int sum(int a, int b){
         return a + b;
     }
@@ -9,8 +11,21 @@ public class functionOverloading{
         return a + b + c;
     }
 
+    //Function overloading using Data Types
+
+    public static int sum1(int a, int b){
+        return a + b;
+    }
+
+    public static float sum1(float a, float b){
+        return a + b;
+    }
+
     public static void main(String args[]){
         System.out.println(sum(50, 60));
         System.out.println(sum(90, 30, 70));
+
+        System.out.println(sum1(34, 56));
+        System.out.println(sum1(56.43f, 78.76f));
     }
 }
