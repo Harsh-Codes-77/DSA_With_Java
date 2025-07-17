6e87e60cb78a369b5cfb675bc5c1383d94c44794
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-17 18:32:00 +0530

    Changes

--- Patterns/src/floydsTriangle.java
+++ Patterns/src/floydsTriangle.java
@@ -5,10 +5,10 @@ public class floydsTriangle{
             for(int j = 1; j<=i; j++){
                 System.out.print(counter+" ");
                 counter++;
-            }
+            };
             System.out.println();
-        }
-    }
+        };
+    };
     public static void main(String args[]){
         floyds_triangle(5);
     }
