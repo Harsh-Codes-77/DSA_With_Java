40f51a88ba108846aa5d57cdf7b7d1a089175757
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-20 10:42:15 +0530

    Hollow Rhombus Pattern

--- Patterns/src/hollowRhombus.java
+++ Patterns/src/hollowRhombus.java
@@ -1,11 +1,15 @@
 public class hollowRhombus{
     public static void hollow_rhombus(int n) {
         for(int i = 1; i <=n; i++){
+
 //            Spaces
+
             for(int j = 1; j <= (n-i); j++){
                 System.out.print(" ");
             }
+
 //           Hollow rectangle - Stars
+
             for(int j = 1; j<=n; j++){
                 if(i == 1 || i == n || j == 1 || j == n) {
                     System.out.print("*");
