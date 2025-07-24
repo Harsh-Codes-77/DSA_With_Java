1eac5b1a22fcf80d74b10124831a99578abd16c1
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-24 19:20:12 +0530

    Accessing Arrays

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -2,9 +2,11 @@ class Main {
     public static void main(String[] args) {
 
         // create an array
+
         int[] age = {12, 4, 5, 2, 5};
 
         // access each array elements
+
         System.out.println("Accessing Elements of Array:");
         System.out.println("First Element: " + age[0]);
         System.out.println("Second Element: " + age[1]);
