3ffe094b9057c796d62e6c7a712ec44307d43770
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-26 10:57:40 +0530

    Array As A Function Argument

--- Arrays/src/arrAsAFunArg.java
+++ Arrays/src/arrAsAFunArg.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class arrAsAFunArg{
     public static void update(int marks[]){
-
+        for(int i = 0; i<marks.length; i++){
+            marks[i] = marks[i] + 1;
+        }
     }
+
 }
