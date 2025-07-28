b6b70853fbfe8fe9d9596123652b7dd61d1ecdb0
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-07-28 22:48:55 +0530

    Initialization of Array

--- Arrays/src/arrayInitialization.java
+++ Arrays/src/arrayInitialization.java
@@ -1,1 +1,10 @@
+public class arrayInitialization {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30};               // literal initialization
+        int[] b = new int[3];                 // new array of size 3 with default zeros
+        b[0] = 5; b[1] = 15; b[2] = 25;      // assign individually
 
+        for (int num : a) System.out.println(num);
+        for (int num : b) System.out.println(num);
+    }
+}
