9a35382ddaa861c43143128104018b8447b53118
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-08-07 08:53:38 +0530

    Multiplication of Matrix

--- Functions/src/primeInRange.java
+++ Functions/src/primeInRange.java
@@ -14,7 +14,7 @@ public class primeInRange{
     public static void primeInRange(int n){
         for(int i = 2; i <= n; i++){
             if(isPrime(i)){
-                System.out.print(i + " ");
+                System.out.print (i + " ");
             }
         }
         System.out.println();
