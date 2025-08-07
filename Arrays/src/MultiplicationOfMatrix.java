13c528f948d702d80cae035ddaf80f1ad65d8ec3
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-08-07 08:54:26 +0530

    Multiplication of Matrix

--- Arrays/src/MultiplicationOfMatrix.java
+++ Arrays/src/MultiplicationOfMatrix.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MatrixMultiplication{
+public class MultiplicationOfMatrix{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
