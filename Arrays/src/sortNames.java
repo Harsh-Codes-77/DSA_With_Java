1880414dce7c5987148739f41fa4b2a2731947f9
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-08-09 17:00:48 +0530

    Sort a given list of names in ascending order.

---
+++ Arrays/src/sortNames.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class SortNames {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // number of names
+        sc.nextLine(); // consume newline
+
+        String[] names = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            names[i] = sc.nextLine();
+        }
+
+        Arrays.sort(names); // case-sensitive sort
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(names[i] + " ");
+        }
+    }
+}
