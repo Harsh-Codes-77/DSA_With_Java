528e406bca8fb7a45d910ebac18608ae129c6a5d
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-08-22 11:47:22 +0530

    Leetcode 217. Contains Duplicate

--- LeetCode/src/containsDuplicate217.java
+++ LeetCode/src/containsDuplicate217.java
@@ -33,10 +33,11 @@
 //        1 <= nums.length <= 105
 //        -109 <= nums[i] <= 109
 
-
+import java.util.*;
 
 class containsDuplicate217{
     public boolean containDuplicate(int[] nums){
+        Arrays.sort(nums);
         for(int i = 1; i<nums.length; i++){
             if(nums[i]  == nums[i - 1]){
                 return true;
