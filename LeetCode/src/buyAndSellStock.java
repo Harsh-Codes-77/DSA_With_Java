c150166064f8e20a11dad89b2b5f7e1e86d0cc28
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-08-23 22:37:13 +0530

    Leetcode 121. Best Time to Buy and Sell Stock

--- LeetCode/src/buyAndSellStock.java
+++ LeetCode/src/buyAndSellStock.java
@@ -25,3 +25,20 @@
 //
 //        1 <= prices.length <= 105
 //        0 <= prices[i] <= 104
+
+class buyAndSellStock{
+    public int maxProfit(int[] prices){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            if(buyPrice < prices[i]){
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }else{
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
