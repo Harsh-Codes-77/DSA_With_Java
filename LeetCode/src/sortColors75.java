901d43aa53dcc4c0197c4540f7cb568401f20802
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-08-24 10:27:03 +0530

    Leetcode 75. Sort Colors

--- LeetCode/src/sortColors75.java
+++ LeetCode/src/sortColors75.java
@@ -1,3 +1,5 @@
+//75. Sort Colors
+
 //Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
 //
 //We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
@@ -21,3 +23,17 @@
 //n == nums.length
 //1 <= n <= 300
 //nums[i] is either 0, 1, or 2.
+
+class sortColors75{
+    public void bubbleSort(int[] nums){
+        for(int i = 0; i < nums.length-1; i++){
+            for(int j = 0; j < nums.length-1-i; j++){
+                if(nums[j]>nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+    }
+}
