1bad5b391abe046809de3b92708825e120784514
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-08-25 18:21:06 +0530

    Counting Sort Technique

--- Sorting Techniques/src/countingSort.java
+++ Sorting Techniques/src/countingSort.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class countingSort{
 
-    static int findMax(int[] arr){
+    /*static int findMax(int[] arr){
         int mx = Integer.MIN_VALUE;
         for(int i = 0; i<arr.length; i++){
             if(arr[i] > mx){
@@ -38,5 +38,39 @@ public class countingSort{
         int[] arr = {1, 4, 5, 2, 2, 5};
         CountingSort(arr);
         display(arr);
+    }*/
+
+    public static void CountingSort(int[] arr){
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length; i++){
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest+1];
+        for(int i = 0; i<arr.length; i++){
+            count[arr[i]]++;
+        }
+//        sorting
+        int j = 0;
+        for(int i = 0; i <count.length; i++){
+            while(count[i] > 0){
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+    public static void printArr(int[] arr){
+        for(int i = 0; i <arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 1, 3, 2};
+        CountingSort(arr);
+        printArr(arr);
     }
 }
+
