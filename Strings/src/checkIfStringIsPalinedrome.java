c571e1ee73795a1f1b0fd31da5f54d282433c947
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-08-30 21:23:11 +0530

    Check if String is palinedrome...

--- Strings/src/checkIfStringIsPalinedrome.java
+++ Strings/src/checkIfStringIsPalinedrome.java
@@ -4,10 +4,14 @@ public class checkIfStringIsPalinedrome{
     public static boolean isPalinedrome(String str){
         for(int i = 0; i <str.length()/2; i++){
             int n = str.length();
-            if(str.charAt(i) == str.charAt(n-i-1))
+            if(str.charAt(i) != str.charAt(n-i-1)){
+                return false;
+            }
         }
+        return true;
     }
     public static void main(String[] args) {
         String str = "racecar";
+        System.out.println(isPalinedrome(str));
     }
 }
