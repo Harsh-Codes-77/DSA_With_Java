34531bd23ac2074d16feeca34bea2bdb84a0459c
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-09-05 21:12:23 +0530

    Leetcode 49. Group Anagrams

--- groupAnagrams49.java
+++ groupAnagrams49.java
@@ -33,3 +33,27 @@
 //        1 <= strs.length <= 104
 //        0 <= strs[i].length <= 100
 //strs[i] consists of lowercase English letters.
+
+
+class groupAnagrams49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List> map = new HashMap<>();
+        for(String s : strs){
+            int[] count = new int[26];
+            for(char c : s.toCharArray()){
+                count[ c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i : count){
+                sb.append("#");
+                sb.append(i);
+            }
+            String key = sb.toString();
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<String>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList(map.values());
+    }
+}
