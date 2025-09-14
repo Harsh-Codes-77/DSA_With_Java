05e2ed0c561f7bebd738d9361046f8746d653f95
harsh <pharshpathak703@gmail.com>
2025-09-14 22:32:28 +0530

    Leetcode 11. Container With Most Water

--- LeetCode/src/leetcode11.java
+++ LeetCode/src/leetcode11.java
@@ -27,3 +27,18 @@
 //n == height.length
 //2 <= n <= 10^5
 //        0 <= height[i] <= 10^4
+
+class leetcode11 {
+    public int maxArea(int[] height) {
+        int res = 0;
+        int i = 0;
+        int j = height.length - 1;
+
+        while(i < j){
+            res = Math.max(res, Math.min(height[i], height[j]) * (j - i));
+            if(height[i] < height[j]) i++;
+            else j--;
+        }
+        return res;
+    }
+}
