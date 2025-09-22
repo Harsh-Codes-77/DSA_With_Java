c235ee566b78718d69365ecd3dd80bc5ecc7184f
harsh <pharshpathak703@gmail.com>
2025-09-22 18:03:20 +0530

    Leetcode 3005. Count Elements With Maximum Frequency

--- LeetCode/src/leetcode3005.java
+++ LeetCode/src/leetcode3005.java
@@ -26,3 +26,19 @@
 //
 //        1 <= nums.length <= 100
 //        1 <= nums[i] <= 100
+
+class leetcodd3005 {
+    public int maxFrequencyElements(int[] nums) {
+        int freq[] = new int[101];
+        int maxFreq = 0;
+        for(int num : nums){
+            freq[num]++;
+            maxFreq = Math.max(maxFreq, freq[num]);
+        }
+        int ans = 0;
+        for(int i = 1; i <= 100; i++){
+            if(freq[i] == maxFreq) ans += maxFreq;
+        }
+        return ans;
+    }
+}
