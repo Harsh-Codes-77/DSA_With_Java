a4274888cf25e1d698716d8c78fc021f196421df
harsh <pharshpathak703@gmail.com>
2025-09-22 18:02:51 +0530

    Leetcode 3005. Count Elements With Maximum Frequency

--- LeetCode/src/leetcode3005.java
+++ LeetCode/src/leetcode3005.java
@@ -1,3 +1,5 @@
+//Leetcode 3005. Count Elements With Maximum Frequency
+
 //You are given an array nums consisting of positive integers.
 //
 //Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
