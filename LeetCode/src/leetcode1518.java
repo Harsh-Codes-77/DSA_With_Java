db43c65aa230f71f06f65b376e6bd4367d9b807b
harsh <pharshpathak703@gmail.com>
2025-10-02 17:36:22 +0530

    Leetcode 1518. Water Bottles

--- LeetCode/src/leetcode1518.java
+++ LeetCode/src/leetcode1518.java
@@ -28,3 +28,19 @@
 //
 //        1 <= numBottles <= 100
 //        2 <= numExchange <= 100
+
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int drinked = 0;
+        int empBottles = 0;
+
+        while(numBottles > 0){
+            drinked += numBottles;
+            empBottles += numBottles;
+
+            numBottles =  empBottles / numExchange;
+            empBottles = empBottles % numExchange;
+        }
+        return drinked;
+    }
+}
